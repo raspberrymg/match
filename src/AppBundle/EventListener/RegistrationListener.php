c806ce8d608f82b1d825693622da424ee5d0ea4b
George Brooks <truckeetrout@yahoo.com>
2015-10-12 14:38:51 -0700

    correct AdminMailer - no FOSUserBundle items

---
+++ src/AppBundle/EventListener/RegistrationListener.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the App package.
+ * 
+ * (c) George W. Brooks
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+//src\AppBundle\EventListener\RegistrationListener
+
+namespace AppBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Description of RegistrationListener
+ *
+ * @author George
+ */
+class RegistrationListener implements EventSubscriberInterface
+{
+
+    private $em;
+    private $mailer;
+    private $tools;
+
+    public function __construct(EntityManager $em, $messager, $tools)
+    {
+        $this->em = $em;
+        $this->mailer = $messager;
+        $this->tools = $tools;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
+        );
+    }
+
+    /**
+     * Persist organization on staff registration success
+     * @param \FOS\UserBundle\Event\FormEvent $event
+     */
+    public function onRegistrationSuccess(FormEvent $event)
+    {
+        /** @var $user \FOS\UserBundle\Model\UserInterface */
+        $user = $event->getForm()->getData();
+        $user->setAddDate(new \DateTime());
+        $type = $this->tools->getUserType($user);
+        if ('staff' === $type) {
+            $organization = $user->getOrganization();
+            $organization->setTemp(true);
+            $user->setOrganization($organization);
+            $this->em->persist($organization);
+            $user->addRole('ROLE_STAFF');
+            $this->mailer->sendNewOrganization($organization);
+        }
+        if ('admin' === $type) {
+            $user->addRole('ROLE_ADMIN');
+        }
+        if ('volunteer' === $type) {
+            $user->setReceiveEmail(true);
+            $user->setEnabled(true);
+        }
+    }
+}
---
+++ src/AppBundle/Tools/AdminMailer.php
@@ -0,0 +1,283 @@
+<?php
+/*
+ * This file is part of the App package.
+ * 
+ * (c) George W. Brooks
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+//src\AppBundle\Tools\AdminMailer.php
+
+namespace AppBundle\Tools;
+
+use AppBundle\Entity\AdminOutbox;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Description of AdminMailer
+ *
+ * @author George
+ */
+class AdminMailer
+{
+    protected $mailer;
+//    protected $router;
+    protected $twig;
+    protected $address;
+
+//    protected $em;
+//
+    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig,
+                                $address, EntityManager $em)
+    {
+        $this->mailer  = $mailer;
+//        $this->router = $router;
+        $this->twig    = $twig;
+        $this->address = $address;
+        $this->em      = $em;
+    }
+
+    /**
+     * Send eblast to selected volunteers re: new opportunity
+     * called by AdminController::showMatchedVolunteersAction()
+     */
+    public function sendNewOppMail($bcc, $opportunity)
+    {
+        $bccAddresses = [];
+        foreach ($bcc as $volunteer) {
+            $bccAddresses[] = $volunteer->getEmail();
+        }
+        $body    = $this->twig->render(
+            'new_opp', array('opportunity' => $opportunity,), 'text/html');
+        $message = \Swift_Message::newInstance()
+            ->setSubject('New opportunity')
+            ->setFrom($this->address)
+            ->setBcc($bccAddresses)
+            ->setContentType('text/html')
+            ->setBody(
+            $body
+            )
+        ;
+
+        $this->mailer->send($message);
+        $recipientCount = $this->recipientCount($message);
+
+        $this->adminUpdateMessage($message);
+
+        return $recipientCount;
+    }
+
+    /**
+     * Send organization alert on expiring opportunities
+     * called by AdminController::expiringAlertsAction()
+     */
+    public function sendExpiringAlerts($expiring)
+    {
+        $recipientCount = 0;
+        foreach ($expiring as $opp) {
+            $addressee = $this->getAddressee($opp['user']);
+            $message   = \Swift_Message::newInstance()
+                ->setSubject('Expiring opportunities')
+                ->setFrom($this->address)
+                ->setTo($addressee)
+                ->setBcc($this->adminRecipients())
+                ->setContentType('text/html')
+                ->setBody(
+                $this->twig->render(
+                    'expiring_opp',
+                    array(
+                    'expiring' => $opp,
+                    ), 'text/html'
+                )
+                )
+            ;
+            $this->mailer->send($message);
+            $recipientCount ++;
+        }
+
+        return $recipientCount;
+    }
+
+    /**
+     * Alert admins to new org being created
+     * called by RegistrationListener::onRegistrationSuccess()
+     */
+    public function sendNewOrganization($organization)
+    {
+        $message = \Swift_Message::newInstance()
+            ->setSubject('New organization')
+            ->setFrom($this->address)
+            ->setTo($this->adminRecipients())
+            ->setContentType('text/html')
+            ->setBody(
+            $this->twig->render(
+                'AppBundle:Email:new_organization.html.twig',
+                array(
+                'organization' => $organization,
+                ), 'text/html'
+            )
+            )
+        ;
+
+        return $this->mailer->send($message);
+    }
+
+    /**
+     * Send notice re: activated organization
+     * called by AdminController::activateOrgAction()
+     */
+    public function activateOrgMail($organization, $to)
+    {
+        $recipient = [];
+        foreach ($to as $user) {
+            $recipient[] = $this->getAddressee($user);
+        }
+        $cc = $this->adminRecipients();
+        if (!empty($recipient)) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Organization now active')
+                ->setFrom($this->address)
+                ->setTo($recipient)
+                ->setCc($cc)
+                ->setContentType('text/html')
+                ->setBody(
+                $this->twig->render(
+                    'activated_org',
+                    array(
+                    'organization' => $organization,
+                    ), 'text/html'
+                )
+            );
+
+            return $this->mailer->send($message);
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Send e-mail re volunteer interest
+     * called by DefaultController::oppFormAction()
+     */
+    public function sendOppInterestEmail($to, $from, $subject, $content)
+    {
+        $recipient = [];
+        foreach ($to as $user) {
+            $recipient[] = $this->getAddressee($user);
+        }
+        if (!empty($recipient)) {
+            $message        = \Swift_Message::newInstance()
+                ->setSubject($subject)
+                ->setFrom($from)
+                ->setTo($recipient)
+                ->setBcc($this->adminRecipients())
+                ->setContentType('text/html')
+                ->setBody(
+                $this->twig->render(
+                    "AppBundle:Default:onlineOppEmailContent.html.twig",
+                    array(
+                    'content' => $content,
+                    'recipient' => $recipient,
+                    ), 'text/html'
+                )
+                )
+            ;
+            $headers        = $message->getHeaders();
+            $headers->addTextHeader('Opportunity-Interest', 'true');
+            $this->mailer->send($message);
+            $recipientCount = $this->recipientCount($message);
+
+            return $recipientCount;
+        } else {
+            return 0;
+        }
+    }
+
+    public function getAddressee($user)
+    {
+        $sandbox = $this->parameters['sandbox'];
+        if ($sandbox) {
+            return $this->parameters;
+        } else {
+            return $user->getEmail();
+        }
+    }
+
+    private function adminRecipients()
+    {
+        $em         = $this->em;
+        $admins     = $em->getRepository("AppBundle:Admin")->findBy(['locked' => false]);
+        $adminEmail = [];
+        foreach ($admins as $admin) {
+            $email        = $admin->getEmail();
+            $adminEmail[] = $email;
+        }
+
+        return $adminEmail;
+    }
+
+    /**
+     * populate AdminOutbox
+     */
+    public function populateAdminOutbox($recipientArray)
+    {
+        if (array_key_exists('function', $recipientArray)) {
+            $outbox = new AdminOutbox();
+            $outbox->setDate(new \DateTime());
+            $outbox->setFunction($recipientArray['function']);
+            $outbox->setMessageType($recipientArray['messageType']);
+            $outbox->setOppId($recipientArray['oppId']);
+            $outbox->setOrgId($recipientArray['orgId']);
+            $outbox->setRecipientId($recipientArray['id']);
+            $outbox->setUserType($recipientArray['userType']);
+            $this->em->persist($outbox);
+        } else {
+            foreach ($recipientArray as $recipient) {
+                $outbox = new AdminOutbox();
+                $outbox->setDate(new \DateTime());
+                $outbox->setFunction($recipient['function']);
+                $outbox->setMessageType($recipient['messageType']);
+                $outbox->setOppId($recipient['oppId']);
+                $outbox->setOrgId($recipient['orgId']);
+                $outbox->setRecipientId($recipient['id']);
+                $outbox->setUserType($recipient['userType']);
+                $this->em->persist($outbox);
+            }
+        }
+        $this->em->flush();
+    }
+
+    private function recipientCount($message)
+    {
+        $to  = count($message->getTo());
+        $cc  = count($message->getCc());
+        $bcc = count($message->getBcc());
+
+        return $to + $cc + $bcc;
+    }
+
+    private function adminUpdateMessage($message)
+    {
+        if ('New opportunity' === $message->getSubject()) {
+            //send e-blast re: volunteers notified
+            $count        = $this->recipientCount($message);
+            $body         = $message->getBody();
+            $adminMessage = \Swift_Message::newInstance()
+                ->setSubject('E-blast results')
+                ->setFrom($this->parameters)
+                ->setTo($this->adminRecipients())
+                ->setContentType('text/html')
+                ->setBody(
+                "$count e-mails have been sent with the following:"."<br>".
+                $body
+                )
+            ;
+
+            $this->mailer->send($adminMessage);
+
+            return;
+        }
+    }
+}
