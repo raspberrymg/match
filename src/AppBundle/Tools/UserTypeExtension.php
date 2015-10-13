af63b2148a111004b627b7e6139d077a6a3d2010
George Brooks <truckeetrout@yahoo.com>
2015-10-13 13:49:16 -0700

    add twig userType extension

---
+++ src/AppBundle/Tools/UserTypeExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the App package.
+ * 
+ * (c) George W. Brooks
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+//src\AppBundle\Tools\UserTypeExtension.php
+
+namespace AppBundle\Tools;
+
+use Symfony\Component\Security\Core\SecurityContext;
+
+/**
+ * Creates Twig "filter" to allow use of user type (as defined in Person entity)
+ *
+ * @author George
+ */
+class UserTypeExtension extends \Twig_Extension
+{
+
+    private $context;
+
+    public function __construct(SecurityContext $context, $tools)
+    {
+        $this->context = $context;
+        $this->tools = $tools;
+    }
+
+    private function getUser()
+    {
+        $user = (is_object($this->context->getToken())) ? $this->context->getToken()->getUser() : 'anon.';
+        return $user;
+    }
+
+    public function getType()
+    {
+        $user = $this->getUser();
+        return $this->tools->getUserType($user);
+    }
+
+        public function getFilters()
+    {
+        return array(
+            'userType' => new \Twig_Filter_Method($this, "getType"),
+        );
+    }
+
+    public function getName()
+    {
+        return 'userType';
+    }
+}
